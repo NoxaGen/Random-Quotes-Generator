27ad5e1e1d6f5ea03821d0e4bc3457cc6f349998
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2022-06-02 16:49:42 +0200

    add some project ideas as comments

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-//loading... learning about clousures atm
+// mysle ze beda 2 niezalezne interwały
+
+//1. bedzie obslugiwac slider z deliaktnym efektme przesuwania w tle, na zasadzie set interval
+//bedzie on niezalezny
+
+//2. quote fo the day - mysle ze jeden cytat powinien byc niezalezny od losowania z kazdym razem jak user otworzy strone 
+//bedzie zmieniac go tylko data - pomyslec jak
+
+//3. random generator bedzie sposrod listy pobranych div z p losowal jeden, dawal mu opcaity na 1 a tamtym zabieral
+//jest taka funkcja w tablicy bodajrze find/indexoff
